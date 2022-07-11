14ce0429860901809c74c2e2863605553d248ab3
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2022-07-11 00:12:50 -0400

    new
    look way better 😳

--- main.lua
+++ main.lua
@@ -1,24 +1,25 @@
-local save = {}
-function save.profile(name)
+local FileAPI = {}
+function FileAPI.profile(name)
     if not isfolder(name) then
         makefolder(name)
     end
 end
 
-function save.save(profile, dataname, data)
+function FileAPI.save(profile, dataname, data)
     if isfolder(profile) then
         writefile(profile.."/"..dataname, data)
     end
 end
 
-function save.load(profile, dataname)
+function FileAPI.load(profile, dataname)
     if isfolder(profile) and isfile(profile.."/"..dataname) then
         return readfile(profile.."/"..dataname)
     end
 end
 
-function save.overwrite(profile, dataname)
+function FileAPI.overwrite(profile, dataname)
     if isfolder(profile) then
         appendfile(profile.."/"..dataname, data)
     end
 end
+return FileAPI
